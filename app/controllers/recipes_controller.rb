5d9e0f77ad66c1893e178e928eac409d9a0f267c
LOctavio <luis_ram1997@hotmail.com>
2022-02-01 14:33:05 -0600

    Display a list of recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,15 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    user = curren_user
+    @recipes = user.recipe.all
+  end
 
   def show; end
+
+  def destroy
+    Recipe.find(params[:id]).destroy
+    redirect_to recipes_path
+  end
 
   def public_recipes; end
 end
