96e0ee632a3941e46fdabb513e32665a8a56b7c8
enmanuel <enmanuellassis@gmail.com>
2022-02-01 16:26:31 -0400

    display food list and create food form

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,8 @@
 class Food < ActiveRecord::Base
   has_many :recipefood
   belongs_to :user
-  validates :name, uniqueness: true {message: 'name is already taken'}, presence: true
-  
+  validates :name, uniqueness: { message: 'name is already taken' }, presence: true
+
   validates :measurement_unit, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
