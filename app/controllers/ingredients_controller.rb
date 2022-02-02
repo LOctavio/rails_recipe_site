b3f987f7fe46f0310224bfd2f7303fa02be3140a
enmanuel <enmanuellassis@gmail.com>
2022-02-02 12:23:39 -0400

    Add new ingredient task

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,10 +2,24 @@ class IngredientsController < ApplicationController
     def index   
     end
     def new
-      recipe_id = params[:recipe_id]
       @foods = Food.all 
-
     end
     def create
+      @recipe_id = params[:recipe_id]
+      received_values = post_params
+      received_values.merge!(recipe_id:@recipe_id)
+      @new_ingredient = Ingredient.create(received_values)
+      if @new_ingredient.save
+        redirect_to recipe_path(:id => @recipe_id)
+      else
+        flash[:notice] = 'Ingredient Not saved, please try again '
+        redirect_to new_ingredient_path(:recipe_id => @recipe_id) 
+      end
+
     end
+    private
+
+  def post_params
+    params.require(:ingredient).permit(:quantity,:food_id)
+  end
 end
