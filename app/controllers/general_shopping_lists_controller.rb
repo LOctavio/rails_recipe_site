86df1f01d1e6baf3017114dfd98d447ac524e87f
enmanuel <enmanuellassis@gmail.com>
2022-02-03 23:55:07 -0400

    Fix errors in shpping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,6 +1,5 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @user = User.new
-    @ingredients = @user.missing_ingredients(current_user.id)
+    @ingredients = current_user.missing_ingredients
   end
 end
